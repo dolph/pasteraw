1bd0fef25d69780b1b77b0bc54124474954f006a
Dolph Mathews <dolph.mathews@gmail.com>
2013-07-03 16:49:32 -0500

    Add a variable route

--- hello.py
+++ hello.py
@@ -15,5 +15,11 @@ def hello():
     return 'Hello World'
 
 
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+
 if __name__ == '__main__':
     app.run()
