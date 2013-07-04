f46dc5b5936e44a518f491d4faad0a17ed815093
Dolph Mathews <dolph.mathews@gmail.com>
2013-07-03 22:41:29 -0500

    Add login / logout tests

--- tests/test_flaskr.py
+++ tests/test_flaskr.py
@@ -1,4 +1,5 @@
 import unittest
+import uuid
 
 import flaskr
 
@@ -11,9 +12,37 @@ class FlaskrTestCase(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_nothing(self):
-        pass
+    def login(self, username):
+        return self.app.post(
+            '/login',
+            data={'username': username},
+            follow_redirects=True)
 
+    def logout(self):
+        return self.app.get(
+            '/logout',
+            follow_redirects=True)
+
+    def test_login_without_username(self):
+        r = self.login('')
+        self.assertIn('Invalid username', r.data)
+
+    def test_login_logout(self):
+        username = uuid.uuid4().hex
+
+        r = self.login(username)
+        self.assertIn('Welcome, %s' % username, r.data)
+
+        r = self.logout()
+        self.assertIn('You were logged out', r.data)
+        self.assertNotIn(username, r.data)
+
+    def test_logout_idempotent(self):
+        r = self.logout()
+        self.assertIn('You were logged out', r.data)
+
+        r = self.logout()
+        self.assertIn('You were logged out', r.data)
 
 if __name__ == '__main__':
     unittest.main()
