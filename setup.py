ad0f9a7a482641612c9313d4bab994fbfc80fadd
Dolph Mathews <dolph.mathews@gmail.com>
2013-07-03 22:34:17 -0500

    Added a setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+
+setuptools.setup(
+    name='flaskr',
+    version='1.0',
+    long_description=__doc__,
+    packages=['flaskr'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['Flask'])
