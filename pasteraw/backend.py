72f6d6b6daa16ca8981bb595ff35754cdf133106
Dolph Mathews <dolph.mathews@gmail.com>
2013-07-26 00:43:33 -0500

    Added bootstrap styling

--- pasteraw/backend.py
+++ pasteraw/backend.py
@@ -2,21 +2,29 @@ import zlib
 
 import redis
 
+import pasteraw
 from pasteraw import base36
 
 
-DB = redis.StrictRedis(host='localhost', port=6379, db=0)
+def make_redis(app):
+    return redis.StrictRedis(
+        host=app.config['REDIS_HOST'],
+        port=app.config['REDIS_PORT'],
+        db=app.config['REDIS_DB'])
+
+
+REDIS = make_redis(pasteraw.app)
 
 
 def save(content):
     compressed = zlib.compress(content)
     key = base36.unique()
-    DB.set(key, compressed)
+    REDIS.set(key, compressed)
     return key
 
 
 def load(key):
-    compressed = DB.get(key)
+    compressed = REDIS.get(key)
     if compressed is None:
         return None
     content = zlib.decompress(compressed)
--- pasteraw/config.py
+++ pasteraw/config.py
@@ -7,5 +7,9 @@ TESTING = False
 # if you don't override the secret key, one will be chosen for you
 SECRET_KEY = uuid.uuid4().hex
 
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_DB = 0
+
 CELERY_BROKER_URL = 'amqp://guest:guest@localhost:5672//'
 CELERY_RESULT_BACKEND = CELERY_BROKER_URL
