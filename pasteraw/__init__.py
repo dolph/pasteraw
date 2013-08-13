8a3e87ff66a2c5a2e242a87d07a28590f756cc4d
Dolph Mathews <dolph.mathews@gmail.com>
2013-08-13 12:39:14 -0500

    make the flask obj available seperately

--- pasteraw/__init__.py
+++ pasteraw/__init__.py
@@ -9,11 +9,14 @@ app.config.from_object('pasteraw.config')
 # override defaults with instance-specific configuration
 app.config.from_pyfile('pasteraw_config.py', silent=True)
 
+application = app
+
+# wrap application if sentry is available
 raven_url = app.config.get('RAVEN_URL', None)
 if raven_url is not None:
     import raven
     from raven import middleware as raven_middleware
-    app = raven_middleware.Sentry(app, client=raven.Client(raven_url))
+    application = raven_middleware.Sentry(app, client=raven.Client(raven_url))
 
 
 import pasteraw.views  # flake8: noqa
