790f185412ae81b984bf10f088bbe87d4877ce41
Dolph Mathews <dolph.mathews@gmail.com>
2014-03-12 11:24:56 -0500

    default log to /tmp

--- pasteraw/config.py
+++ pasteraw/config.py
@@ -3,7 +3,7 @@ import uuid
 
 DEBUG = False
 TESTING = False
-LOG_FILE = '/var/log/pasteraw.log'
+LOG_FILE = '/tmp/pasteraw.log'
 
 # if you don't override the secret key, one will be chosen for you
 SECRET_KEY = uuid.uuid4().hex
