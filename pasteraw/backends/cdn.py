ed689faa151edec36d29513e0c49730b4c07b2df
Dolph Mathews <dolph.mathews@gmail.com>
2015-02-25 13:23:03 +0000

    add some hacky debug

--- pasteraw/backends/cdn.py
+++ pasteraw/backends/cdn.py
@@ -11,6 +11,8 @@ ENABLED = False
 
 if pasteraw.app.config['CLOUD_ID_TYPE'] == 'rackspace':
     pyrax.set_setting('identity_type', pasteraw.app.config['CLOUD_ID_TYPE'])
+    print('Logging into rackspace as %s ...' %
+          pasteraw.app.config['RACKSPACE_USERNAME'])
     pyrax.set_credentials(
         pasteraw.app.config['RACKSPACE_USERNAME'],
         pasteraw.app.config['RACKSPACE_API_KEY'])
@@ -22,6 +24,7 @@ else:
     raise Exception(
         'No credential type provided for CDN services (CLOUD_ID_TYPE).')
 
+print('Setting region to %s' % pasteraw.app.config['CLOUD_REGION'])
 pyrax.set_setting('region', pasteraw.app.config['CLOUD_REGION'])
 pyrax.set_setting('use_servicenet', True)
 
