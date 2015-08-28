ea5502ce8c055f142850e0c1e2ff6539b6e56cb5
Dolph Mathews <dolph.mathews@gmail.com>
2015-08-28 22:46:00 +0000

    use app instead of pasteraw.app

--- pasteraw/cdn.py
+++ pasteraw/cdn.py
@@ -1,41 +1,41 @@
 import pyrax
 import pyrax.exceptions
 
-import pasteraw
+from pasteraw import app
 
 
 ENABLED = False
 
-if pasteraw.app.config['CLOUD_ID_TYPE'] == 'rackspace':
+if app.config['CLOUD_ID_TYPE'] == 'rackspace':
     try:
-        pyrax.set_setting('identity_type', pasteraw.app.config['CLOUD_ID_TYPE'])
+        pyrax.set_setting('identity_type', app.config['CLOUD_ID_TYPE'])
 
-        pasteraw.app.logger.info(
-            'Setting region to %s' % pasteraw.app.config['CLOUD_REGION'])
-        pyrax.set_setting('region', pasteraw.app.config['CLOUD_REGION'])
+        app.logger.info(
+            'Setting region to %s' % app.config['CLOUD_REGION'])
+        pyrax.set_setting('region', app.config['CLOUD_REGION'])
         pyrax.set_setting('use_servicenet', True)
 
-        pasteraw.app.logger.info(
+        app.logger.info(
             'Logging into rackspace as %s ...' %
-            pasteraw.app.config['RACKSPACE_USERNAME'])
+            app.config['RACKSPACE_USERNAME'])
         pyrax.set_credentials(
-            pasteraw.app.config['RACKSPACE_USERNAME'],
-            pasteraw.app.config['RACKSPACE_API_KEY'])
+            app.config['RACKSPACE_USERNAME'],
+            app.config['RACKSPACE_API_KEY'])
 
         # list containers to exercise credentials
         pyrax.cloudfiles.list_containers()
 
         ENABLED = True
     except pyrax.exceptions.PyraxException as e:
-        pasteraw.app.logger.warning(
+        app.logger.warning(
             'Unable to authenticate using pyrax: %s' % e)
 
-elif pasteraw.app.config['CLOUD_ID_TYPE'] == 'keystone':
+elif app.config['CLOUD_ID_TYPE'] == 'keystone':
     raise NotImplementedError(
         'pyrax does not document how to provide keystone credentials '
         '"directly".')
 else:
-    pasteraw.app.logger.warning(
+    app.logger.warning(
         'No credential type provided for CDN services (CLOUD_ID_TYPE).')
 
 
@@ -45,9 +45,9 @@ def upload(key, content):
 
     try:
         container = pyrax.cloudfiles.get_container(
-            pasteraw.app.config['CDN_CONTAINER_NAME'])
+            app.config['CDN_CONTAINER_NAME'])
     except pyrax.exceptions.ClientException as e:
-        pasteraw.app.logger.warning(e)
+        app.logger.warning(e)
         return False
 
     try:
@@ -55,6 +55,6 @@ def upload(key, content):
         obj.change_content_type('text/plain; charset="utf-8"')
         return True
     except pyrax.exceptions.ClientException as e:
-        pasteraw.app.logger.warning(e)
+        app.logger.warning(e)
 
     return False
