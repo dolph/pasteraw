3ad12a2ac40e2f6f2d05bdc105e5379694488e08
Dolph Mathews <dolph.mathews@gmail.com>
2017-04-25 15:57:52 -0500

    Add app.yaml and hello world http service

--- main.go
+++ main.go
@@ -1,3 +1,16 @@
 package main
 
+import (
+    "fmt"
+    "net/http"
+)
+
+func init() {
+    http.HandleFunc("/", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, "Hello, world!")
+}
+
 func main() {}
