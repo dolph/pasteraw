a416d618cd05b64dd9cba7394fdf03a55d7697e1
Dolph Mathews <dolph.mathews@gmail.com>
2017-04-25 23:27:50 -0500

    Move response into struct, and assertions into methods

--- main_test.go
+++ main_test.go
@@ -14,7 +14,7 @@ type TestHandler struct {
 }
 
 // Build an HTTP request, pass it to the HTTP handler, and return the response.
-func (handler TestHandler) request(method string, path string, headers map[string]string) *httptest.ResponseRecorder {
+func (handler TestHandler) request(method string, path string, headers map[string]string) TestResponse {
     request, err := http.NewRequest(method, path, nil)
     if err != nil {
         handler.t.Fatal(err)
@@ -27,33 +27,46 @@ func (handler TestHandler) request(method string, path string, headers map[strin
 
     response := httptest.NewRecorder()
     http.HandlerFunc(handler.f).ServeHTTP(response, request)
-    return response
+    return TestResponse{handler.t, response}
 }
 
 // Make a GET request to the HTTP handler, and return the response.
-func (handler TestHandler) GET(path string, headers map[string]string) *httptest.ResponseRecorder {
+func (handler TestHandler) GET(path string, headers map[string]string) TestResponse {
     return handler.request("GET", path, headers)
 }
 
 // Make a POST request to the HTTP handler, and return the response.
-func (handler TestHandler) POST(path string, headers map[string]string) *httptest.ResponseRecorder {
+func (handler TestHandler) POST(path string, headers map[string]string) TestResponse {
     return handler.request("POST", path, headers)
+}
+
+type TestResponse struct {
+    t *testing.T
+
+    // The HTTP response being asserted against.
+    r *httptest.ResponseRecorder
+}
+
+func (response TestResponse) AssertStatusEquals(expected int) {
+    if response.r.Code != expected {
+        response.t.Errorf(
+            "Handler returned wrong status code: got %v want %v",
+            response.r.Code, expected)
+    }
+}
+
+func (response TestResponse) AssertBodyEquals(expected string) {
+    if actual := response.r.Body.String(); actual != expected {
+        response.t.Errorf(
+            "Handler returned unexpected body: got %v want %v",
+            actual, expected)
+    }
 }
 
 func TestIndexHandler(t *testing.T) {
     handler := TestHandler{t, IndexHandler}
     response := handler.GET("/", nil)
 
-    if status := response.Code; status != http.StatusOK {
-        t.Errorf(
-            "Handler returned wrong status code: got %v want %v",
-            status, http.StatusOK)
-    }
-
-    expected_body := "a plaintext pastebin service"
-    if response.Body.String() != expected_body {
-        t.Errorf(
-            "Handler returned unexpected body: got %v want %v",
-            response.Body.String(), expected_body)
-    }
+    response.AssertStatusEquals(http.StatusOK)
+    response.AssertBodyEquals("a plaintext pastebin service")
 }
