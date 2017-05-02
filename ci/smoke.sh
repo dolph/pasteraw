7cbf0b3820663e62eb20902afbf84278d1a24239
Dolph Mathews <dolph.mathews@gmail.com>
2017-05-02 13:19:12 -0500

    Switch to using network tools image

--- ci/smoke.sh
+++ ci/smoke.sh
@@ -3,11 +3,4 @@ set -ex
 
 HOSTNAME=$1
 
-apt-get update
-apt-get install -y \
-    dnsutils \
-    curl \
-    ;
-
-nslookup $HOSTNAME
 curl https://$HOSTNAME/ | grep "a plaintext pastebin service"
